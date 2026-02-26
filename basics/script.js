ffb7a3ad961e7001af69445b8e5c27a87577c7fa
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-26 18:16:37 +0530

    async and await

--- basics/script.js
+++ basics/script.js
@@ -21,3 +21,16 @@ console.log(obj.name);
 console.log(obj['age']);
 obj.freeze(age);//it remains 20 not able to change
 
+function add(a,b,c,d){
+
+}
+console.log(add.length);//gives the number of parameter present in the function
+
+async function abcd(){
+    var blob = await fetch(`https://randomuser.me/api/`);
+    var data = await blob.json();
+    console.log(data.results);
+}
+abcd();
+
+
