193325e1a45a2a351389a04f37971ce45f791f3c
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-26 17:59:29 +0530

    Objects

--- basics/script.js
+++ basics/script.js
@@ -12,3 +12,12 @@ const filteredArr=arr.filter(function(val){
     return val%2==0;
 })
 console.log(filteredArr);
+
+var obj={
+    name:"Shachin",
+    age:20
+}
+console.log(obj.name);
+console.log(obj['age']);
+obj.freeze(age);//it remains 20 not able to change
+
