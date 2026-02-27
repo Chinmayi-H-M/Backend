af36d3b0721f62c41d418c9b5f7bfe770bd7f897
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-02-27 20:06:37 +0530

    Nodejs rename and copy file

--- nodejs/script.js
+++ nodejs/script.js
@@ -1,17 +1,33 @@
 const fs =require('fs');
 
-fs.writeFile("hey.text","Hello this is from growthDiary",function(err){
-    if(err){
-        console.log(err);
-    }else{
-        console.log("File created successfully");
-    }
-})
+// fs.writeFile("hey.text","Hello this is from growthDiary",function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("File created successfully");
+//     }
+// })
 
-fs.appendFile("hey.text","It covers all the info about tech, health",function(err){
+// fs.appendFile("hey.text","It covers all the info about tech, health",function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("The info updated successfully");
+//     }
+// })
+// fs.rename("hey.text","newHey.text",function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("The file renamed successfully");
+//     }
+// })
+
+fs.copyFile("newHey.text","./copy/hey.text",function(err){
     if(err){
         console.log(err);
-    }else{
-        console.log("The info updated successfully");
+    }
+    else{
+        console.log("The file copied successfully")
     }
 })
