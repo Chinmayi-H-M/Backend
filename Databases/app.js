f01ddab1079a03e0a182570e4c3043b1f23d73d0
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-11 18:54:49 +0530

    CRUD OPERATIONS

--- Databases/app.js
+++ Databases/app.js
@@ -6,11 +6,22 @@ const userModel = require('./usermodule');
 app.get('/',(req,res)=>{
    res.send('hello world');
 });
-app.get('/create',(req,res)=>{
-   userModel.create({
+app.get('/create',async (req,res)=>{
+   let createdUser =await userModel.create({
     name:'harsh',
     email:'hmschinmayi-6@gmail.com',
-    username:'harsh'
+    userName:'harsh'
    })
+   res.send(createdUser);
 });
+
+app.get('/update',async (req,res)=>{
+    let updatedUser=await userModel.findOneAndUpdate({name:'harsh'},{name:'john'}, {new: true},)
+    res.send(updatedUser);
+})
+
+app.get('/read',async (req,res)=>{
+    let users =await userModel.find();
+    res.send(users);
+})
 app.listen(3000);
