d04d1d8a62a0fbc4983f8420a0256bdbdd80fa34
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-12 17:46:00 +0530

    CRUD OPERATIONS

---
+++ Databases/db.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path =require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.get('/',(req,res)=>{
+    res.send('hello');
+})
+
+app.listen(3000);
