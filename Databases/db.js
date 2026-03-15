fa5408248ef256d5d7d39bf6b887140915e052b4
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-15 13:12:09 +0530

    Password decrypt and encrypt

--- Databases/db.js
+++ Databases/db.js
@@ -2,14 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path =require('path');
 const userModel = require('./models/user');
+const cookieParser =require('cookie-parser');
+const bcrypt =require('bcryt');
 
 
 const app = express();
+app.use(cookirParser());
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
+app.get('/', function(req,res){
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+        // Store hash in your password DB.
+    });
+});
+})
 
 app.get('/',(req,res)=>{
     res.render('index');
@@ -35,7 +45,7 @@ app.get('/edit/:id',async (req,res)=>{
 app.post('/update/:id',async (req,res)=>{
     let {name,email,image} =req.body;
     let users = await userModel.findOneAndUpdate({_id: req.params.id},{name,image,email})
-    res.redirect('/read',{users: users});
+    res.redirect('/read');
 })
 app.get('/delete/:id', async (req,res)=>{
     let users = await userModel.findOneAndDelete({_id: req.params.id});
