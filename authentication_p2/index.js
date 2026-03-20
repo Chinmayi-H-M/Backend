c57c8b2d8eedd87343e2824981c5f8f41c4ff1e9
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-20 23:43:13 +0530

    Database integration

--- authentication_p2/index.js
+++ authentication_p2/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app=express();
+const userModel = require('./models/user');
 
 const path = require('path');
 const cookieParser = require('cookie-parser');
