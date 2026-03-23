c3ecb20b35f80c51e75e56e091cd6c3c5076650c
Chinmayi-H-M <hmschinmayi06@gmail.com>
2026-03-23 15:14:56 +0530

    testingdb

--- data_mongo/app.js
+++ data_mongo/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/data_mongo");
+const userModel = require("./modules/user");
+const postModel = require("./modules/posts");
 
 app.get("/",function(req,res){
     res.send("Hello");
 })
+
+app.get("/create",async function(req,res){
+    let user = await userModel.create({
+        username:"Chinmayi",
+        age:19,
+        email:"c@gmail.com",
+        posts:[]
+    })
+    res.json(user);
+})
+
+app.listen(3000);
